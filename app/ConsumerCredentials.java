85781e0236305cfa01136bcb97b1be07244c82a9
Ian McKellar <ian@mckellar.org>
2011-09-19 21:35:32 -0700

    load consumer key/secret from environment variables

---
+++ app/ConsumerCredentials.java
@@ -0,0 +1,12 @@
+public abstract class ConsumerCredentials {
+  // you can get these by signing up for a developer account at:
+  // http://developer.rdio.com/
+  public static String RDIO_CONSUMER_KEY = "";
+  public static String RDIO_CONSUMER_SECRET = "";
+
+  static {
+    // read the consumer key and secret from environment variables
+    ConsumerCredentials.RDIO_CONSUMER_KEY = System.getenv("RDIO_CONSUMER_KEY");
+    ConsumerCredentials.RDIO_CONSUMER_SECRET = System.getenv("RDIO_CONSUMER_SECRET");
+  }
+}
--- app/ConsumerCredentials_EXAMPLE.java
+++
@@ -1,6 +0,0 @@
-public abstract class ConsumerCredentials_EXAMPLE {
-  // you can get these by signing up for a developer account at:
-  // http://developer.rdio.com/
-  public static final String RDIO_CONSUMER_KEY = "";
-  public static final String RDIO_CONSUMER_SECRET = "";
-}
